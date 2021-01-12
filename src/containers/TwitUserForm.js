c1a190f85ea9b70b89652b1c5ccbee4a47f4b387
Tom Schrimshaw <tschrimshaw@gmail.com>
2021-01-12 17:33:09 +0000

    feat: add loading spinner
    placed in bootstrap spinner to indicate loading of tweets.
    Update bootstrap to v4.5 to access spinners.

--- src/containers/TwitUserForm.js
+++ src/containers/TwitUserForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { connect } from 'react-redux'
 import { handleUserInfo } from '../actions/shared'
 import { clearFaves } from '../actions/favorites'
@@ -6,22 +6,26 @@ import { clearFaves } from '../actions/favorites'
 const TwitUserForm = (props) => {
     const { handleUserInfo, clearFaves, errorCallBack } = props
     const [userName, setUserName] = useState('')
+    const spinner = useRef(null)
 
     /*when submitting a user name search dispatch clearFaves to clear the favesList,
     in case there is an error therefore not showing an unrelated list*/
     const handleSubmit = (evt) => {
         evt.preventDefault()
         clearFaves()
+        spinner.current.hidden = false
 
         //thunk action returns promise to get info, has it's own .then when successful to populate store state
         handleUserInfo(userName)
         .then(() => {
             setUserName('')
+            spinner.current.hidden = true
         })
         .catch(err => {
             //error caught from thunk action fires callback to display alert in app.js
             console.log('error getting user data', err)
             errorCallBack(true)
+            spinner.current.hidden = true
         })
     }
 
@@ -33,7 +37,7 @@ const TwitUserForm = (props) => {
                     type='text'
                     className='form-control'
                     value={ userName }
-                    placeholder='Twitter username'
+                    placeholder='Enter Twitter username'
                     onChange={ (evt) => setUserName(evt.target.value) }
                 />
                 <button 
@@ -44,6 +48,11 @@ const TwitUserForm = (props) => {
                     SUBMIT
                 </button>
             </form>
+            <div className='d-flex justify-content-center'>
+                <div className='spinner-border' role='status' hidden={ false } ref={ spinner }>
+                    <span className='sr-only'>Loading...</span>
+                </div>
+            </div>
         </div>
     )
 }
