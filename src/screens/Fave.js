d1f69ab683bf10daebde94899736cf70fb33b454
Tom Schrimshaw <tschrimshaw@gmail.com>
2021-01-15 19:12:19 +0000

    feat: add image to fave obj
    if an image url is present in the data,
    embed the image(s).

--- src/screens/Fave.js
+++ src/screens/Fave.js
@@ -3,7 +3,8 @@ import UserInfo from './UserInfo'
 
 const Fave = (props) => {
     //grab wanted parameters from passed in data prop for tweet
-    const { full_text, created_at, user } = props.data
+    const { full_text, created_at, user, entities } = props.data
+
     const [dateText, setDateText] = useState('')
 
     useEffect(() => {
@@ -18,10 +19,19 @@ const Fave = (props) => {
         }
     }
 
+    //TODO: remove media url from text via regexand media -> url params
+
     return(
         <div className='container border border-dark rounded mb-3 p-2'>
             <UserInfo userData={ user } />
             <p className='pl-5' dangerouslySetInnerHTML={ createMarkup() }></p>
+            {
+                entities.media !== undefined &&(
+                    entities.media.map(image => (
+                        <img className='w-100 rounded' src={ image.media_url } alt='tweet embeded media' key={ image } />
+                    ))
+                )
+            }
             <span className='pl-5'>{ dateText }</span>
         </div>
     )
