c9f9f8509afe178f2fb4fb5ab2fa13e2e20b2e6d
Tom Schrimshaw <tschrimshaw@gmail.com>
2021-02-18 12:24:19 +0000

    chore: first heroku set up
    changed fetch paths and start scripts commands, may need to change again

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,7 @@
+//const devPath = 'http://localhost:3001'
 //async call the twitter api to gey a specific user
 const getTwitLikes = async (userName) => {
-   const res = await fetch(`http://localhost:3001/getTwits/${userName}`)
+   const res = await fetch(`/getTwits/${userName}`)
     
     try {
         const data = await res.json()
@@ -11,7 +12,7 @@ const getTwitLikes = async (userName) => {
 }
 
 const getTwitUser = async (userName) => {
-    const res = await fetch(`http://localhost:3001/getUser/${userName}`)
+    const res = await fetch(`/getUser/${userName}`)
 
     try {
         const user = await res.json()
